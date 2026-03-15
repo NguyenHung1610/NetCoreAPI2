f741d0ef2d5572b9774ccf085dd5e7152e2483c7
NguyenHung1610 <Nguyenhung161003@gmail.com>
2026-03-15 16:02:29 +0700

    Lesson6

--- Models/Student/Student.cs
+++
@@ -1,9 +0,0 @@
-namespace DemoMVC.Models.Student
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string? MSV { get; set; }
-        public string? FullName { get; set; }
-    }
-}
